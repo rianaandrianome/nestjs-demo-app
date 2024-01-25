2abfec7370e1fcb8b95fa41ae11afe513bc02e9a
riana andrianome <riana@rianas-Laptop.local>
2024-01-25 20:01:38 +0300

    Fix prettier

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-
   const p1 = await prisma.product.upsert({
     where: { name: 'Macbook Air laptop' },
     update: {},
@@ -12,22 +11,22 @@ async function main() {
       name: 'Macbook Air laptop',
       description: 'This is the description for Macbook air laptop',
       images: [
-        "https://m.media-amazon.com/images/I/71N+DK0pEaL._AC_SX569_.jpg",
-        "https://reviewed-com-res.cloudinary.com/image/fetch/s--LS2KHGFp--/b_white,c_fill,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_547,q_auto,w_972/https://reviewed-production.s3.amazonaws.com/1607081088000/DSC_0877.jpg"
+        'https://m.media-amazon.com/images/I/71N+DK0pEaL._AC_SX569_.jpg',
+        'https://reviewed-com-res.cloudinary.com/image/fetch/s--LS2KHGFp--/b_white,c_fill,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_547,q_auto,w_972/https://reviewed-production.s3.amazonaws.com/1607081088000/DSC_0877.jpg',
       ],
     },
   });
 
   const p2 = await prisma.product.upsert({
-    where: { name: "Samsung S22 Android" },
+    where: { name: 'Samsung S22 Android' },
     update: {},
     create: {
-      name: "Samsung S22 Android",
+      name: 'Samsung S22 Android',
       description: 'This is the description for the Samsung S22',
       images: [
-        "https://fdn.gsmarena.com/imgroot/news/22/02/samsung-galaxy-s22-hot-take/inline/-1200w5/gsmarena_003.jpg",
-        "https://guycom.mg/wp-content/uploads/2022/03/LD0005928708_1_0005928719_0005928744.jpg"
-      ]
+        'https://fdn.gsmarena.com/imgroot/news/22/02/samsung-galaxy-s22-hot-take/inline/-1200w5/gsmarena_003.jpg',
+        'https://guycom.mg/wp-content/uploads/2022/03/LD0005928708_1_0005928719_0005928744.jpg',
+      ],
     },
   });
 
